da214c51cfa7e39be9ea42d8b8ab54b0daa534a4
Ahmet Baş <ahba8184@gmail.com>
2023-04-27 21:52:13 +0300

    Update AtmProject.java

--- src/main/java/AtmProject.java
+++ src/main/java/AtmProject.java
@@ -1,6 +1,8 @@
 public class AtmProject {
     public static void main(String[] args) {
         System.out.println("Atm Projesi");
-        System.out.println("Version 2 değişikliği");
+        System.out.println("Version 02 değişikliği");
+        
+        System.out.println("Version 03 değişikliği");
     }
 }
